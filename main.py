da01253c8bd19351714cc0380363e69a4525360e
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-20 13:59:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 noun1 = input('Please input a Singular Noun: (example: Bird)        ').lower()
-noun2= input('Please input a Plural Noun: (example: Chickens)        ').lower()
-noun3= input('Please input a Singular Noun: (example: Bird)        ').lower()
+noun2 = input('Please input a Plural Noun: (example: Chickens)        ').lower()
+noun3 = input('Please input a Singular Noun: (example: Bird)        ').lower()
 verb1 = input('Please input a Verb: (example: Jump)        ').lower()
 adjective = input('Please input an adjective: (example: Flowery)        ').lower()
 greeting = input('Please input a greeting: (example: Hello again)        ').upper()
@@ -10,3 +10,9 @@ job = input('Please input a place of work: (example: Factory)       ')
 madlibs = f'{greeting}! my name is {name} and this is my morning routine: Every morning I {verb1}, get out of my {adjective} {noun1}, and then get dressed for the day. Then I eat {noun2}, brush my {noun3}, get ready for my job at the {job}, get in my car and start my day.'
 
 print(madlibs)
+
+
+# alt + click | Add multiple cursers 
+# alt + up/down | Move line up/down 
+# alt + up/down + Shift | Clone Up/down 
+# shift + direction | Select area 
